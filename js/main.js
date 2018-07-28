d721b3271e046f56198e8182732cf9fd5e5aec85
Tom Eloe <tomeloe@gmail.com>
2018-07-28 12:55:05 -0700

    added navbar

--- js/main.js
+++ js/main.js
@@ -1,10 +1,43 @@
 $(function() {
   
   // Smooth scrolling using jQuery easing
-  $("#learn-more").click(function() {
+  
+  $("#nav-about").click(function() {
     $('html, body').animate({
-      scrollTop: $("#projects").offset().top
+      scrollTop: $("#about").offset().top - 53
     }, 1000, "easeInOutExpo");
   });
+
+  $("#nav-experience").click(function() {
+    $('html, body').animate({
+      scrollTop: $("#experience").offset().top - 53
+    }, 1000, "easeInOutExpo");
+  });
+
+  $("#nav-projects").click(function() {
+    $('html, body').animate({
+      scrollTop: $("#projects").offset().top - 53
+    }, 1000, "easeInOutExpo");
+  });
+
+  $("#nav-contact").click(function() {
+    $('html, body').animate({
+      scrollTop: $("#contact").offset().top - 53
+    }, 1000, "easeInOutExpo");
+  });
+
+
+  $("#learn-more").click(function() {
+    $('html, body').animate({
+      scrollTop: $("#about").offset().top - 53
+    }, 1000, "easeInOutExpo");
+  });
+
+
+
+
+
+
+
 
 });
