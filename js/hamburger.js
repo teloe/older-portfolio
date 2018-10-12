ed57d750fc27e73c43653ffe09d47c47ecbda9da
Tom Eloe <tomeloe@gmail.com>
2018-10-12 14:07:22 -0700

    update

--- js/hamburger.js
+++ js/hamburger.js
@@ -3,5 +3,10 @@ $(function(){
   $('#nav-icon').click(function(){
     $(this).toggleClass('open');
   });
+  
+  $('.nav-link').click(function(){
+    $('.collapse').collapse('hide');
+    $(this).toggleClass('open');
+});
 
 });
